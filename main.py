99ba091a4d93b6676f3a4fd0b8ccc6be6946f486
Z6 <whitedeathcross@gmail.com>
2019-07-30 23:17:01 +0900

    add the CORS back

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import datetime
 import logging
 import os
 import json
 
 from flask import Flask
+from flask_cors import CORS
 import sqlalchemy
 
 app = Flask(__name__)
@@ -29,22 +16,17 @@ db_pass = os.environ.get("DB_PASS")
 db_name = os.environ.get("DB_NAME")
 cloud_sql_connection_name = os.environ.get("CLOUD_SQL_CONNECTION_NAME")
 
-def connect_to_cloudsql():
-    # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
-    # will be set to 'Google App Engine/version'.
-    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
-        db = sqlalchemy.create_engine(os.environ['SQLALCHEMY_DATABASE_URI'])
-    else:
-        db = sqlalchemy.create_engine(
-            sqlalchemy.engine.url.URL(
-                drivername='mysql+pymysql',
-                host="127.0.0.1",
-                username=db_user,
-                password=db_pass,
-                database=db_name
-            )
+db = sqlalchemy.create_engine(
+    sqlalchemy.engine.url.URL(
+        drivername='mysql+pymysql',
+        username=db_user,
+        password=db_pass,
+        database=db_name,
+        query={
+            'unix_socket': '/cloudsql/{}'.format(cloud_sql_connection_name)
+            }
         )
-    return db
+    )
 
 @app.route('/photo', methods=['GET'])
 def index():
