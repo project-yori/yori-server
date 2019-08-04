ccec5dcbd79618d43e9abdd249e44f2fa31839ac
dwi2 <dwi2@dwi2.com>
2019-08-04 17:09:14 +0900

    Update connetion string and REAMDE to make local env work

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 ### Prerequisite
 
 - Python 3
+- [Cloud SQL Proxy](https://cloud.google.com/sql/docs/mysql/sql-proxy)
 
 ### Run Development Server
 
@@ -26,6 +27,30 @@
   pip install -r requirements.txt
   ```
 
+- Install and launch [Cloud SQL Proxy](https://cloud.google.com/sql/docs/mysql/sql-proxy)
+
+  ```sh
+  cloud_sql_proxy -dir=/tmp/cloudsql -instances=yori_instance_name=tcp:3306
+  ```
+
+- Launch server
+
+  ```sh
+  env FLASK_APP=main.py DB_USER=username DB_PASS=password DB_NAME=database_name CLOUD_SQL_CONNECTION_NAME=yori_instance_name flask run
+  ```
+
+  or if you want to enable network access to local server (especially when you are developing yori-view)
+
+  ```sh
+  env FLASK_APP=main.py DB_USER=username DB_PASS=password DB_NAME=database_name CLOUD_SQL_CONNECTION_NAME=yori_instance_name flask run --host=0.0.0.0
+  ```
+
+  If setting environments bothers you, write them in your dotfiles.
+
+## Deploy
+
+Before deploy to gcloud, you have to install gcloud sdk. ([Install guide](https://cloud.google.com/sdk/docs/quickstarts))
+
 - Create a `secret.yaml` (if you haven't) and set your own environment variable
   **NOTICE: do not upload your `secret.yaml` to repository**
 
@@ -37,22 +62,6 @@
     DB_NAME: your-database
     SLACK_WEBHOOK_URL: slack-webhook-url
   ```
-
-- Run
-
-  ```sh
-  env FLASK_APP=main.py flask run
-  ```
-
-  or if you want to enable network access to local server (especially when you are developing yori-view)
-
-  ```sh
-  env FLASK_APP=main.py flask run --host=0.0.0.0
-  ```
-
-## Deploy
-
-Before deploy to gcloud, you have to install gcloud sdk. ([Install guide](https://cloud.google.com/sdk/docs/quickstarts))
 
 - Deploy Command
 
--- main.py
+++ main.py
@@ -8,18 +8,14 @@ from flask_cors import CORS
 import sqlalchemy
 import requests
 
+
+def onGAE():
+    return os.getenv('GAE_ENV', '').startswith('standard')
+
+
 app = Flask(__name__)
 CORS(app)
 app.config['JSON_AS_ASCII'] = False
-
-
-if os.getenv('GAE_ENV') == 'standard':
-    import google.cloud.logging
-    client = google.cloud.logging.Client('yori-server')
-    client.setup_logging(logging.INFO)
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
 
 db_user = os.environ.get('DB_USER')
 db_pass = os.environ.get('DB_PASS')
@@ -27,24 +23,39 @@ db_name = os.environ.get('DB_NAME')
 cloud_sql_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
 slack_webhook_url = os.environ.get('SLACK_WEBHOOK_URL')
 
-db = sqlalchemy.create_engine(
-    sqlalchemy.engine.url.URL(
+
+if onGAE():
+    import google.cloud.logging
+    client = google.cloud.logging.Client('yori-server')
+    client.setup_logging(logging.INFO)
+    connection_string = sqlalchemy.engine.url.URL(
         drivername='mysql+pymysql',
         username=db_user,
         password=db_pass,
         database=db_name,
         query={
             'unix_socket': '/cloudsql/{}'.format(cloud_sql_connection_name)
-        }
+        })
+else:
+    connection_string = sqlalchemy.engine.url.URL(
+        drivername='mysql+pymysql',
+        username=db_user,
+        password=db_pass,
+        database=db_name
     )
-)
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+db = sqlalchemy.create_engine(connection_string)
 
 
 # TODO:
 # We should create a package and move this into __init__.py of that package
 def notify():
     logger.info('will notify {}'.format(slack_webhook_url))
-    print('will notify {}'.format(slack_webhook_url))
     if slack_webhook_url:
         project_id = os.environ.get('GOOGLE_CLOUD_PROJECT')
         gae_version = os.environ.get('GAE_VERSION')
@@ -65,7 +76,9 @@ def index():
 
 # TODO:
 # We should create a package and move this into __init__.py of that package
-notify()
+if onGAE():
+    notify()
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
